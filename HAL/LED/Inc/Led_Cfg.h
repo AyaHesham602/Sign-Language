eb1af44b0b8f3acd8b45f8d2bf652572e801c119
AyaHesham602 <93055247+AyaHesham602@users.noreply.github.com>
2022-03-08 21:50:16 +0200

    Create Led_Cfg.h

---
+++ HAL/LED/Inc/Led_Cfg.h
@@ -0,0 +1,19 @@
+/*
+ * Led_Cfg.h
+ *
+ *      Author: Aya Hesham
+ */
+
+#ifndef LED_CFG_H_
+#define LED_CFG_H_
+
+#define LED0_PORT  DIO_PORTC
+#define LED0_PIN   DIO_PIN2
+
+#define LED1_PORT  DIO_PORTC
+#define LED1_PIN   DIO_PIN7
+
+#define LED2_PORT  DIO_PORTD
+#define LED2_PIN   DIO_PIN3
+
+#endif /* LED_CFG_H_ */
